22045c5570904e965c5fc8a8f0ea98c147dc5722
Saidakmal Oktamov <70756496+M1racle-Heen@users.noreply.github.com>
2023-03-20 16:46:22 +0600

    Response

--- ResponseFolder/Response.go
+++ ResponseFolder/Response.go
@@ -1,1 +1,11 @@
 package ResponseFolder
+
+type RequestData struct {
+	Date         string       `json:"date,omitempty"`
+	Version      int          `json:"type,omitempty"`
+	Currency     int          `json:"currency,omitempty"`
+	Lang         string       `json:"lang,omitempty"`
+	SenderCity   SenderCity   `json:"from_location"`
+	ReceiverCity ReceiverCity `json:"to_location"`
+	Packages     []Size       `json:"packages"`
+}
