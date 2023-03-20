e41bf4e3313e059186c7547ec6a34424682f2c04
Saidakmal Oktamov <70756496+M1racle-Heen@users.noreply.github.com>
2023-03-20 17:04:46 +0600

    Update cdek_task.go

--- cdek_task.go
+++ cdek_task.go
@@ -1,1 +1,17 @@
 package cdekCostCalculator
+
+type Client struct {
+	Account string `json:"client_id"`
+	Secret  string `json:"client_secret"`
+	Token   string `json:"access_token"`
+	URL     string `json:"api_url"`
+}
+
+func NewClient(account string, securePassword string, apiURL string) *Client {
+	c := &Client{
+		Account: account,
+		Secret:  securePassword,
+		URL:     apiURL,
+	}
+	return c
+}
